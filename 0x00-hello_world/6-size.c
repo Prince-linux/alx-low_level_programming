82784823f209e1efdd65f166cceb42320a667410
Prince Boamah <regioths@gmail.com>
2023-03-16 21:14:58 +0000

    updated scripts

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  *  * main - Entry point
  *   *
@@ -19,6 +18,5 @@ int main(void)
     printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
     printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
     printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-    return (0);
-                                                
+    return (0);                                                
 }
