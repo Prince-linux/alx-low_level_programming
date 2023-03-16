fd1fde4abbf93a7ede3dd6ee438704e6e52e397e
Prince Boamah <regioths@gmail.com>
2023-03-16 21:20:41 +0000

    updated script

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,8 +3,7 @@
  *  * main - Entry point
  *   *
  *    * Return: Always 0 (Success)
- *     
-*/
+ * */
 int main(void)
 {
 int a;
@@ -17,5 +16,5 @@ printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-return (0);                                                
+return (0);
 }
